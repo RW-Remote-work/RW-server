7f2dcb574637df51dc13828af5b224fd99717843
Jinhan He <jhhe@avantifytech.com>
2023-02-28 23:01:58 +0800

    Add BadRequestProblem.java

---
+++ src/main/java/com/rwws/rwserver/exception/BadRequestProblem.java
@@ -0,0 +1,17 @@
+package com.rwws.rwserver.exception;
+
+import org.zalando.problem.AbstractThrowableProblem;
+
+import java.net.URI;
+
+import static org.zalando.problem.Status.BAD_REQUEST;
+
+public class BadRequestProblem extends AbstractThrowableProblem {
+    public BadRequestProblem() {
+        super(null, null, BAD_REQUEST, BAD_REQUEST.getReasonPhrase());
+    }
+
+    public BadRequestProblem(URI type, String title) {
+        super(type, title, BAD_REQUEST, BAD_REQUEST.getReasonPhrase());
+    }
+}
