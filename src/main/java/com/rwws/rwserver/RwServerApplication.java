b61cf43c900491e9154c3d34c12aecb7f9bec7c7
Jinhan He <jhhe@avantifytech.com>
2023-02-28 18:09:31 +0800

    Init project

---
+++ src/main/java/com/rwws/rwserver/RwServerApplication.java
@@ -0,0 +1,13 @@
+package com.rwws.rwserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RwServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rwws/rwserver/RwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwws.rwserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
