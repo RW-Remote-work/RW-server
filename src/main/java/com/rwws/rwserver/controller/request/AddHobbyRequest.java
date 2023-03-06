1ee1724a1448f8f42b3981914f3cb8dd949a3419
Jinhan He <jhhe@avantifytech.com>
2023-03-06 18:30:14 +0800

    Polish

--- src/main/java/com/rwws/rwserver/controller/request/AddHobbyRequest.java
+++ src/main/java/com/rwws/rwserver/controller/request/AddHobbyRequest.java
@@ -2,7 +2,10 @@ package com.rwws.rwserver.controller.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+
 @Data
 public class AddHobbyRequest {
+    @Max(value = 10)
     private String name;
 }
