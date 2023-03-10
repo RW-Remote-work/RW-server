bc885d21e12b465c250a6ba12c631d485d36d7b1
beiding92@163.com <beiding92@163.com>
2023-03-10 23:28:30 +0800

    1、UserController更新

--- src/main/java/com/rwws/rwserver/module/system/user/controller/UserController.java
+++ src/main/java/com/rwws/rwserver/module/system/user/controller/UserController.java
@@ -1,16 +1,9 @@
 package com.rwws.rwserver.module.system.user.controller;
 
-import com.rwws.rwserver.module.support.annotation.WebOperateLog;
-import com.rwws.rwserver.module.system.login.domain.request.RegisterRequest;
-import com.rwws.rwserver.module.system.login.domain.response.RegisterResponse;
-import com.rwws.rwserver.module.system.login.service.RegisterService;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-@WebOperateLog
 public class UserController {
 
 
